340b3d2ba1af32e7a90b06aceca74ec0db233342
mikunilabo <k-wada@mikunilabo.com>
2018-12-11 16:52:50 +0900

    * productionにおいてアセットにバージョニングしておく * ベンダーライブラリを抽出

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,12 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css')
-   .sourceMaps();
+    .extract(['vue']);
+
+mix.sass('resources/sass/app.scss', 'public/css');
+
+mix.sourceMaps();
+
+if (mix.inProduction()) {
+    mix.version();
+}

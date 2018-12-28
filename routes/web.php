514371ae98c971b5ffeaf881533784e46ef9ac40
mikunilabo <k-wada@mikunilabo.com>
2018-12-28 15:06:00 +0900

    * URL階層構造変更 * 全デモルート仮定義

--- routes/web.php
+++ routes/web.php
@@ -16,40 +16,70 @@ Auth::routes(['verify' => true]);
 Route::prefix('/')->middleware(['auth'/* 'verified'*/])->group(function () {
     Route::view('/', $name = 'home')->name($name);
 
-    Route::view($name = 'colors', $name)->name($name);
-    Route::view($name = 'typography', $name)->name($name);
-
-    Route::prefix($prefix = 'base')->name(sprintf('%s.', $prefix))->group(function () use ($prefix) {
-        Route::view(sprintf('%s.%s', $prefix, $name = 'breadcrumb'), $name)->name($name);
-        Route::view(sprintf('%s.%s', $prefix, $name = 'cards'), $name)->name($name);
-        Route::view(sprintf('%s.%s', $prefix, $name = 'carousel'), $name)->name($name);
-        Route::view(sprintf('%s.%s', $prefix, $name = 'collapse'), $name)->name($name);
-        Route::view(sprintf('%s.%s', $prefix, $name = 'forms'), $name)->name($name);
-        Route::view(sprintf('%s.%s', $prefix, $name = 'jumbotron'), $name)->name($name);
-        Route::view(sprintf('%s.%s', $prefix, $name = 'list_group'), $name)->name($name);
-        Route::view(sprintf('%s.%s', $prefix, $name = 'navs'), $name)->name($name);
-        Route::view(sprintf('%s.%s', $prefix, $name = 'pagination'), $name)->name($name);
-        Route::view(sprintf('%s.%s', $prefix, $name = 'popovers'), $name)->name($name);
-        Route::view(sprintf('%s.%s', $prefix, $name = 'progress'), $name)->name($name);
-        Route::view(sprintf('%s.%s', $prefix, $name = 'scrollspy'), $name)->name($name);
-        Route::view(sprintf('%s.%s', $prefix, $name = 'switches'), $name)->name($name);
-        Route::view(sprintf('%s.%s', $prefix, $name = 'tables'), $name)->name($name);
-        Route::view(sprintf('%s.%s', $prefix, $name = 'tabs'), $name)->name($name);
-        Route::view(sprintf('%s.%s', $prefix, $name = 'tooltips'), $name)->name($name);
+    /**
+     * Theme
+     */
+    Route::prefix($prefix = 'theme')->name(sprintf('%s.', $prefix))->group(function () use ($prefix) {
+        Route::view($name = 'colors', sprintf('%s.%s', $prefix, $name))->name($name);
+        Route::view($name = 'typography', sprintf('%s.%s', $prefix, $name))->name($name);
     });
 
-    Route::prefix($prefix = 'pages')->name(sprintf('%s.', $prefix))->group(function () {
-        Route::view($name = 'login', sprintf('auth.%s', $name), ['demo' => true])->name($name);
-        Route::view($name = 'register', sprintf('auth.%s', $name), ['demo' => true])->name($name);
+    /**
+     * Components
+     */
+    Route::prefix($prefix = 'components')->name(sprintf('%s.', $prefix))->group(function () use ($prefix) {
+        Route::view($name = 'charts', sprintf('%s.%s', $prefix, $name))->name($name);
+        Route::view($name = 'widgets', sprintf('%s.%s', $prefix, $name))->name($name);
 
-        Route::prefix($prefix = 'password')->name(sprintf('%s.', $prefix))->group(function () {
-            Route::view($name = 'request', 'auth.passwords.email', ['demo' => true])->name($name);
-            Route::view($name = 'reset', 'auth.passwords.reset', ['demo' => true, 'token' => null])->name($name);
+        Route::prefix($prefix2 = 'base')->name(sprintf('%s.', $prefix2))->group(function () use ($prefix, $prefix2) {
+            Route::view($name = 'breadcrumb', sprintf('%s.%s.%s', $prefix, $prefix2, $name))->name($name);
+            Route::view($name = 'cards', sprintf('%s.%s.%s', $prefix, $prefix2, $name))->name($name);
+            Route::view($name = 'carousel', sprintf('%s.%s.%s', $prefix, $prefix2, $name))->name($name);
+            Route::view($name = 'collapse', sprintf('%s.%s.%s', $prefix, $prefix2, $name))->name($name);
+            Route::view($name = 'forms', sprintf('%s.%s.%s', $prefix, $prefix2, $name))->name($name);
+            Route::view($name = 'jumbotron', sprintf('%s.%s.%s', $prefix, $prefix2, $name))->name($name);
+            Route::view($name = 'list_group', sprintf('%s.%s.%s', $prefix, $prefix2, $name))->name($name);
+            Route::view($name = 'navs', sprintf('%s.%s.%s', $prefix, $prefix2, $name))->name($name);
+            Route::view($name = 'pagination', sprintf('%s.%s.%s', $prefix, $prefix2, $name))->name($name);
+            Route::view($name = 'popovers', sprintf('%s.%s.%s', $prefix, $prefix2, $name))->name($name);
+            Route::view($name = 'progress', sprintf('%s.%s.%s', $prefix, $prefix2, $name))->name($name);
+            Route::view($name = 'scrollspy', sprintf('%s.%s.%s', $prefix, $prefix2, $name))->name($name);
+            Route::view($name = 'switches', sprintf('%s.%s.%s', $prefix, $prefix2, $name))->name($name);
+            Route::view($name = 'tables', sprintf('%s.%s.%s', $prefix, $prefix2, $name))->name($name);
+            Route::view($name = 'tabs', sprintf('%s.%s.%s', $prefix, $prefix2, $name))->name($name);
+            Route::view($name = 'tooltips', sprintf('%s.%s.%s', $prefix, $prefix2, $name))->name($name);
         });
 
-        Route::prefix($prefix = 'errors')->name(sprintf('%s.', $prefix))->group(function () use ($prefix) {
-            Route::view($name = '404', sprintf('%s.%s', $prefix, $name), ['demo' => true])->name($name);
-            Route::view($name = '500', sprintf('%s.%s', $prefix, $name), ['demo' => true])->name($name);
+        Route::prefix($prefix = 'buttons')->name(sprintf('%s.', $prefix))->group(function () use ($prefix) {
+            //
+        });
+
+        Route::prefix($prefix = 'icons')->name(sprintf('%s.', $prefix))->group(function () use ($prefix) {
+            //
+        });
+
+        Route::prefix($prefix = 'notifications')->name(sprintf('%s.', $prefix))->group(function () use ($prefix) {
+            //
+        });
+    });
+
+    /**
+     * Extras
+     */
+    Route::prefix($prefix = 'extras')->name(sprintf('%s.', $prefix))->group(function () {
+        Route::prefix($prefix = 'pages')->name(sprintf('%s.', $prefix))->group(function () {
+            Route::view($name = 'login', sprintf('auth.%s', $name), ['demo' => true])->name($name);
+            Route::view($name = 'register', sprintf('auth.%s', $name), ['demo' => true])->name($name);
+
+            Route::prefix($prefix = 'password')->name(sprintf('%s.', $prefix))->group(function () {
+                Route::view($name = 'request', 'auth.passwords.email', ['demo' => true])->name($name);
+                Route::view($name = 'reset', 'auth.passwords.reset', ['demo' => true, 'token' => null])->name($name);
+            });
+
+            Route::prefix($prefix = 'errors')->name(sprintf('%s.', $prefix))->group(function () use ($prefix) {
+                Route::view($name = '404', sprintf('%s.%s', $prefix, $name), ['demo' => true])->name($name);
+                Route::view($name = '500', sprintf('%s.%s', $prefix, $name), ['demo' => true])->name($name);
+            });
         });
     });
 });
